695ab5b80e0122a33d282046315650049a1b21e4
trv004 <trv004@ucsd.edu>
2025-05-16 19:16:03 -0700

    added map

---
+++ map.js
@@ -0,0 +1,16 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJ2MDA0IiwiYSI6ImNtYXJreGVidDBiemEycW9lbHVjM2MzOXgifQ.CpD32zXiKB11g-D4X_hkTQ';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
